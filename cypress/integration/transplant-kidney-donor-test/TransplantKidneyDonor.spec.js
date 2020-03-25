7ee8ce4ae557a48eef69bea464ca6fc41378d71c
VladimirSokurenko <dp240191svv1@gmail.com>
2020-03-26 00:15:13 +0200

    added-demographics-test

--- cypress/integration/transplant-kidney-donor-test/TransplantKidneyDonor.spec.js
+++ cypress/integration/transplant-kidney-donor-test/TransplantKidneyDonor.spec.js
@@ -1,4 +1,5 @@
 import landingPage from "../../paje-object/LandingPage";
+import demographicsPage from "../../paje-object/DemographicsPage";
 
 describe("transplan kidney donor page test", () => {
   before(() => {
@@ -47,11 +48,11 @@ describe("transplan kidney donor page test", () => {
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.dayField)
-      .type('01')
+      .type("01")
       .get(landingPage.monthField)
-      .type('01')
+      .type("01")
       .get(landingPage.yearField)
-      .type('1991')
+      .type("1991")
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.validationErrorMsg)
@@ -62,41 +63,111 @@ describe("transplan kidney donor page test", () => {
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.yourHeightFeetFiled)
-      .should('be.visible')
+      .should("be.visible")
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.validationErrorMsg)
       .first()
-      .should('have.text', 'Not a whole number')
+      .should("have.text", "Not a whole number")
       .get(landingPage.yourHeightFeetFiled)
-      .type('190')
+      .type("190")
       .get(landingPage.yourHeightInchesFiled)
-      .type('190')
+      .type("190")
       .get(landingPage.yourWeightFiled)
-      .type('80')
+      .type("80")
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.primaryPhoneFiled)
-      .should('be.visible')
+      .should("be.visible")
       .get(landingPage.nextBtn)
       .click()
       .get(landingPage.validationErrorMsg)
       .first()
-      .should('have.text', 'Required')
+      .should("have.text", "Required")
       .get(landingPage.primaryPhoneFiled)
-      .type('555-55-55')
+      .type("555-55-55")
       .get(landingPage.yourPhoneNumberTypeSelect)
-      .select('Mobile')
+      .select("Mobile")
       .get(landingPage.yourEmailAddressFiled)
-      .type('test@test.com')
+      .type("test@test.com")
+      .get(landingPage.nextBtn)
+      .click();
+    cy.contains("Donor Type")
+      .should("be.visible")
+      .get(landingPage.iKnowRecipientRadioBtn)
+      .should("be.visible")
+      .get(landingPage.iDontKnowRecipientRadioBtn)
+      .should("be.visible")
+      .get(landingPage.nextBtn)
+      .should("be.visible");
+  });
+
+  it("should check the Demographics page elements", () => {
+    var headingText = "Please enter your Race:";
+    var americanIndianAlaskaText = "American Indian or Alaska Native";
+    var arabianText = "Arabian";
+    var blackAfricanAmericanText = "Black or African American";
+    var whiteCaucasianText = "White/Caucasian";
+    var asianText = "Asian";
+    var middleEasternNorthAfricanText = "Middle Eastern or North African";
+    var nativeHawaiianPacificText = "Native Hawaiian or Other Pacific Islander";
+    var otherText = "Other";
+    var hispanicOriginHeading = "Are you of Hispanic origin?YesNo";
+    var preferedLanguageHeading = "What is your preferred language?";
+    var languages = [
+      "English",
+      "Spanish",
+      "Vietnamese",
+      "Mandarin",
+      "Arabic",
+      "Creole",
+      "Portuguese",
+      "Other"
+    ];
+    var otherLanguageHeading = "If Other:";
+    var doYouNeedTranslatorHeading = "Do you need a translator?YesNo";
+    cy.visit("/race.0")
+      .get(landingPage.startSurveyBtn)
+      .click()
+      .get(demographicsPage.headingText)
+      .should("have.text", headingText)
+      .get(demographicsPage.aindCheckBox)
+      .should("have.text", americanIndianAlaskaText)
+      .get(demographicsPage.arabianCheckBox)
+      .should("have.text", arabianText)
+      .get(demographicsPage.afroCheckBox)
+      .should("have.text", blackAfricanAmericanText)
+      .get(demographicsPage.whiteCheckBox)
+      .should("have.text", whiteCaucasianText)
+      .get(demographicsPage.asianCheckBox)
+      .should("have.text", asianText)
+      .get(demographicsPage.middleeasternCheckBox)
+      .should("have.text", middleEasternNorthAfricanText)
+      .get(demographicsPage.isalnderCheckBox)
+      .should("have.text", nativeHawaiianPacificText)
+      .get(demographicsPage.otherCheckBox)
+      .should("have.text", otherText)
+      .get(demographicsPage.hispanicQuestionBar)
+      .should("have.text", hispanicOriginHeading)
+      .get(demographicsPage.preferedLanguageBar)
+      .then(el => {
+        assert.include(el.text(), preferedLanguageHeading);
+      });
+    for (var i = 0; i < languages.length; i++) {
+      cy.get(demographicsPage.languageSelector)
+        .select(languages[i])
+        .should("be.visible");
+    }
+    cy.get(demographicsPage.otherLanguageBar)
+      .should("have.text", otherLanguageHeading)
+      .get(demographicsPage.otherLanguageField)
+      .should("be.visible")
+      .get(demographicsPage.doYouNeedTransltorBar)
+      .should("have.text", doYouNeedTranslatorHeading)
       .get(landingPage.nextBtn)
       .click()
-    cy.contains('Donor Type').should('be.visible')
-      .get(landingPage.iKnowRecipientRadioBtn)
-      .should('be.visible')
-      .get(landingPage.iDontKnowRecipientRadioBtn)
-      .should('be.visible')
-      .get(landingPage.nextBtn)
-      .should('be.visible')
+      .get(landingPage.validationErrorMsg)
+      .first()
+      .should("have.text", "Required");
   });
 });
